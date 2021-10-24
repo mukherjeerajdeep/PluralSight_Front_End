1140dbadde37edab4d90242b509f5f1cd0e4b5e1
Rajdeep Mukherjee <rajdeep.mukherjee@ericsson.com>
2021-10-24 16:16:39 +0200

    JS-Add-Event-Listener

--- Front-End Web Development Quick Start With HTML5, CSS, and JavaScript/WorkSheet/index.js
+++ Front-End Web Development Quick Start With HTML5, CSS, and JavaScript/WorkSheet/index.js
@@ -35,3 +35,28 @@ function showMessage(message) {
 function clearMessage(){
     formInfo.innerHTML = "";
 }
+
+// Add event for the buttons 
+const contactForm = document.getElementById("contact");
+contactForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    showMessage("Sending your message...");
+});
+
+// function sendMessage() {
+//     showMessage("Sending your message...");
+// }
+
+// Add events for collections 
+const experiences = 
+    document.getElementsByClassName("experience"); //Returns collections instead of the items
+
+for (let x = 0; x < experiences.length; x++){
+    const item = experiences[0];
+    item.addEventListener("mouseenter", function(event){
+        event.target.style = "background-color : #808080;";
+    });
+    item.addEventListener("mouseleave", function(event){
+        event.target.style = "";
+    });
+}
